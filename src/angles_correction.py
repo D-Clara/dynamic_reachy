b5f2d520114e2c1e50965dcc87bde465c3e71197
DIEUDONNE Clara <cdieudonn001@enseirb-matmeca.fr>
2023-02-05 19:51:57 +0100

    correction calcul du couple du moteur de l'épaule

--- src/angles_correction.py
+++ src/angles_correction.py
@@ -14,8 +14,8 @@ def actif_angles_correction_empty_hand(shoulder_angle, elbow_angle):
     l_arm = 0.293
     m_forearm = 0.6
     m_arm = 0.6
-    shoulder_torque = -g*(m_arm*l_arm/2*np.sin(shoulder_angle*np.pi/180)+m_forearm*l_forearm/2*np.sin((shoulder_angle+elbow_angle)*np.pi/180)+m_forearm*l_arm*np.sin(shoulder_angle*np.pi/180)) 
-    elbow_torque = -g*m_forearm*l_forearm/2*np.sin(elbow_angle*np.pi/180)
+    shoulder_torque = -g*((m_arm * l_arm/2+ m_forearm * l_arm)*np.sin(shoulder_angle * np.pi/180) + m_forearm * l_forearm/2 *np.sin((shoulder_angle + elbow_angle) * np.pi/180))
+    elbow_torque = -g * m_forearm *l_forearm/2 * np.sin((shoulder_angle + elbow_angle) * np.pi/180)
     # shoulder_angle += -0.86139*shoulder_torque+0.348547*sign(shoulder_angle)
     shoulder_angle += -0.8298166*shoulder_torque
     # elbow_angle += -1.54423*elbow_torque-0.504432
