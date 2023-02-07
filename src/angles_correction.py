b7db981f837271d54e23565b96fd40dff4a6e74e
GuiFornes <fornesguillaume@gmail.com>
2023-02-07 09:20:27 +0100

    merge

--- src/angles_correction.py
+++ src/angles_correction.py
@@ -14,8 +14,8 @@ def actif_angles_correction_empty_hand(shoulder_angle, elbow_angle, init_torque_
     l_arm = 0.293
     m_forearm = 0.6
     m_arm = 0.6
-    shoulder_torque = -g*((m_arm * l_arm/2+ m_forearm * l_arm)*np.sin(shoulder_angle * np.pi/180) + m_forearm * l_forearm/2 *np.sin((shoulder_angle + elbow_angle) * np.pi/180))
-    elbow_torque = -g * m_forearm *l_forearm/2 * np.sin((shoulder_angle + elbow_angle) * np.pi/180)
+    shoulder_torque = init_torque_shoulder-g*((m_arm * l_arm/2+ m_forearm * l_arm)*np.sin(shoulder_angle * np.pi/180) + m_forearm * l_forearm/2 *np.sin((shoulder_angle + elbow_angle) * np.pi/180))
+    elbow_torque = init_torque_elbow-g * m_forearm *l_forearm/2 * np.sin((shoulder_angle + elbow_angle) * np.pi/180)
     shoulder_angle += -0.86139*shoulder_torque+0.348547*sign(shoulder_angle)
     elbow_angle += -1.54423*elbow_torque-0.504432
     return [shoulder_angle, elbow_angle]
