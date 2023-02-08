410cfd867e11f5ae550ab4ccf2f0bacf8209ebab
GuiFornes <fornesguillaume@gmail.com>
2023-02-08 18:34:58 +0100

    results and experiment

--- tst/test_dynamixel.py
+++ tst/test_dynamixel.py
@@ -1,5 +1,8 @@
 import pypot.dynamixel as dm
 import time
+import numpy as np
+
+# Connect to the usb port and dynamixel "controller"
 ports = dm.get_available_ports()
 if not ports:
     exit('No port')
@@ -7,6 +10,7 @@ dxl_io = dm.DxlIO(ports[0])
 print(ports)
 print(dxl_io)
 
+# Search for the available motors
 motor_id = []
 for i in range(50):
     if dxl_io.ping(i):
@@ -14,23 +18,40 @@ for i in range(50):
 print(motor_id)
 ids = dxl_io.scan(motor_id)
 
+######################################################
+# Get the current position of the motors connected
+######################################################
 # while True:
 #     print(dxl_io.get_present_position(ids))
 #     time.sleep(1)
 
-dxl_io.set_goal_position({motor_id[0]: 0})
+######################################################
+# Put the fisrt connected motor to 0 degrees
+######################################################
+# dxl_io.set_goal_position({motor_id[0]: 0})
 
-# dxl_io.set_joint_mode(motor_id)
-# for i in range(10):
-#     dxl_io.set_goal_position({motor_id[0]: -45})
-#     time.sleep(1)
-#     dxl_io.set_goal_position({motor_id[0]: 19})
-#     time.sleep(1)
+######################################################
+# Test on the wrist motor
+######################################################
+angles = np.linspace(-45, 19, 50)
+dxl_io.set_joint_mode(motor_id)
+for i in range(10):
+    for j in angles:
+        dxl_io.set_goal_position({motor_id[0]: j})
+        time.sleep(0.01)
+    time.sleep(0.5)
+    for j in angles[::-1]:
+        dxl_io.set_goal_position({motor_id[0]: j})
+        time.sleep(0.01)
+    time.sleep(0.5)
 
-print("done")
-
+######################################################
+# Compliant test on a motor
+######################################################
 # dxl_io.set_torque_limit({motor_id: 100})
 # time.sleep(0.5)
 # dxl_io.set_goal_position({motor_id: 0})
 # time.sleep(1)
 # dxl_io.set_torque_limit({motor_id: 0})
+
+print("done")
